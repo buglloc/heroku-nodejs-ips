2e26e0319954fdf865aad198627cbf148c220331
Andrew Krasichkov <buglloc@yandex.ru>
2018-06-22 09:39:44 +0300

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .set('trust proxy', 1)  // http://expressjs.com/en/guide/behind-proxies.html
+  .get('/', (req, res) => {
+    let out = `
+req.ip: ${req.ip}
+req.ips: ${req.ips}
+`;
+    res.send(out);
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
