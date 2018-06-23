ffd80e667b35639d1662fc5065c031562debbb71
Andrew Krasichkov <buglloc@yandex.ru>
2018-06-23 17:02:27 +0300

    .

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const PORT = process.env.PORT || 5000;
 express()
   .set('trust proxy', 1)  // http://expressjs.com/en/guide/behind-proxies.html
   .get('/', (req, res) => {
-    let out = `
-req.ip: ${req.ip}
-req.ips: ${req.ips}
-`;
+    let out = `req.ip: ${req.ip}`;
     res.type('text/plain');
     res.send(out);
   })
